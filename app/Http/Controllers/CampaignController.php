ab5073733a4c78a9996638cfc84ac21e68a5ef64
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-09 16:24:28 +0100

    implementer la methode index

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -4,20 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Campaign;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Exception;
 
 class CampaignController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        try {
+            $campaigns = Campaign::with('newsletter')->get();
+            return response()->json(['status' => 'success', 'data' => $campaigns], 200);
+        } catch (Exception $e) {
+            return response()->json(['status' => 'error', 'message' => 'Erreur lors de la récupération des campagnes', 'error' => $e->getMessage()], 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
