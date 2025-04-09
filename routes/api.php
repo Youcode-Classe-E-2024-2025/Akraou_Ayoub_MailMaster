6ce9efd0d83f9dd4913e0f7fa11c4df7a80a1c77
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-09 16:23:40 +0100

    ajouter l'endpoint apiRecources /campaigns

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CampaignController;
 use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\SubscriberController;
 use Illuminate\Http\Request;
@@ -17,6 +18,7 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 Route::apiResource('subscribers', SubscriberController::class);
 Route::apiResource('newsletters', NewsletterController::class);
+Route::apiResource('campaigns', CampaignController::class);
 
 Route::get('/docs', function () {
     return redirect('/vendor/swagger-ui/index.html?url=/docs/swagger.yaml');
